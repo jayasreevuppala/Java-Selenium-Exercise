072cb2773ba97227ead3f8674e0af293d3c4f9f4
bobpozun <robert.pozun@gmail.com>
2019-02-27 13:50:30 -0500

    Update readme. Add some helpful imports. Rename browser to driver.

--- src/test/java/BasicTestClass.java
+++ src/test/java/BasicTestClass.java
@@ -3,26 +3,31 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BasicTestClass {
-    private static WebDriver browser;
+    private static WebDriver driver;
 
     @BeforeEach
     public void beforeEach() {
         // if not on windows, update chromedriver directory below
         System.setProperty("webdriver.chrome.driver", "webdrivers\\chromedriver_win32\\chromedriver.exe");
-        browser = new ChromeDriver();
+        driver = new ChromeDriver();
     }
     @Test
     public void basicTest1() {
 
         // write code here:
 
-
     }
     @AfterEach
     public void afterEach() {
-        browser.close();
+        driver.close();
     }
 }
